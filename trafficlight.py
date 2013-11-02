6a5b3ba4ba62549ef1829bf237b0fa9478414d3b
Scott Martin <smartin015@gmail.com>
2013-11-02 15:26:49 -0400

    Updated traffic light simulation - something's off in the display, but the sim works just fine

--- trafficlight.py
+++ trafficlight.py
@@ -7,13 +7,10 @@ def LED(C, I):
   color = C.extra['col']
   if (I != C.extra['c']):
     color = tuple([0.3*col for col in color])
-  pygame.draw.circle(C.extra['screen'], color, C.extra['pos'], 10, 0);
+  pygame.draw.circle(C.extra['screen'], color, C.extra['pos'], 10, 0)
 
 @ckt
 def LEDController(C):
-    if C.read('en') == "G": #init thing - wait a bit before starting
-      yield C.Wait(1)
-
     while True:
         while C.read('en') != "R":  #while other light is active
           yield {"out": "R"}
@@ -34,7 +31,6 @@ def stoplight(id, wire, enable, window, x, y):
     LED(src(I=wire), extra(screen=window, pos=(x,y), c="R", col=(255,0,0)))
     LED(src(I=wire), extra(screen=window, pos=(x,y+20), c="Y", col=(255,255,0)))
     LED(src(I=wire), extra(screen=window, pos=(x,y+40), c="G", col=(0,255,0)))
-    
     LEDController(
       src(en=enable), 
       dest(out=wire)
@@ -47,17 +43,17 @@ def main():
     
     window = pygame.display.set_mode((120, 60)) 
     net("en1", "en2")
-    net_set("en1", "G")
+    net_set("en1", "G") #This one starts
+    net_set("en2", "R")
     stoplight(1, "en1", "en2", window, 10, 10)
     stoplight(2, "en2", "en1", window, 30, 10)
-
    
     n = 0
     while True:
         sleep(0.1)
         step()
         pygame.display.flip()
-        print n
+        print ckt_state()
         n = n + 1
 
 
